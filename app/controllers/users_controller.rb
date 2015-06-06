ef9d13310735c15b1dc5d9950589de285358bf67
ikhakoo <ikhakoo@gmail.com>
2015-06-06 19:57:39 -0400

    updating facebook api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
 
   def login
     @user = User.koala(request.env['omniauth.auth']['credentials'])
-		u = User.find_or_create_by(facebook_id: @user.entries[2][1])
-		u.name = @user.entries[0][1]
+		u = User.find_or_create_by(facebook_id: @user.entries[0][1])
+		u.name = @user.entries[1][1] + " " + @user.entries[3][1]
 		u.save!
+		binding.pry
     @pins = Pin.all
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   def self.koala(auth)
     access_token = auth['token']
     facebook = Koala::Facebook::API.new(access_token)
-    facebook.get_object("me?fields=name,picture")
+    public_attributes = facebook.get_object("me")
   end
 end
