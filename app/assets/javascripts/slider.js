c1ee8b0f41eb39583f12313d53c24876035b7f84
Courtney Lee <courtneylee2113@gmail.com>
2015-06-06 15:06:42 -0400

    fixed slider

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,35 +1,80 @@
-var currentIndex = 0,
-  items = $('.container div'),
-  itemAmt = items.length;
+var ul;
+var li_items;
+var imageNumber;
+var imageWidth;
+var prev, next;
+var currentPostion = 0;
+var currentImage = 0;
 
-function cycleItems() {
-  var item = $('.container div').eq(currentIndex);
-  items.hide();
-  item.css('display','inline-block');
+
+function init(){
+  ul = document.getElementById('image_slider');
+  li_items = ul.children;
+  imageNumber = li_items.length;
+  imageWidth = li_items[0].children[0].clientWidth;
+  ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
+  prev = document.getElementById("prev");
+  next = document.getElementById("next");
+  //.onclike = slide(-1) will be fired when onload;
+  /*
+  prev.onclick = function(){slide(-1);};
+  next.onclick = function(){slide(1);};*/
+  prev.onclick = function(){ onClickPrev();};
+  next.onclick = function(){ onClickNext();};
 }
 
-var autoSlide = setInterval(function() {
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-}, 3000);
+function animate(opts){
+  var start = new Date;
+  var id = setInterval(function(){
+    var timePassed = new Date - start;
+    var progress = timePassed / opts.duration;
+    if (progress > 1){
+      progress = 1;
+    }
+    var delta = opts.delta(progress);
+    opts.step(delta);
+    if (progress == 1){
+      clearInterval(id);
+      opts.callback();
+    }
+  }, opts.delay || 17);
+  //return id;
+}
 
-$('.next').click(function() {
-  clearInterval(autoSlide);
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-});
+function slideTo(imageToGo){
+  var direction;
+  var numOfImageToGo = Math.abs(imageToGo - currentImage);
+  // slide toward left
 
-$('.prev').click(function() {
-  clearInterval(autoSlide);
-  currentIndex -= 1;
-  if (currentIndex < 0) {
-    currentIndex = itemAmt - 1;
-  }
-  cycleItems();
-});
+  direction = currentImage > imageToGo ? 1 : -1;
+  currentPostion = -1 * currentImage * imageWidth;
+  var opts = {
+    duration:1000,
+    delta:function(p){return p;},
+    step:function(delta){
+      ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
+    },
+    callback:function(){currentImage = imageToGo;}  
+  };
+  animate(opts);
+}
+
+function onClickPrev(){
+  if (currentImage == 0){
+    slideTo(imageNumber - 1);
+  }     
+  else{
+    slideTo(currentImage - 1);
+  }   
+}
+
+function onClickNext(){
+  if (currentImage == imageNumber - 1){
+    slideTo(0);
+  }   
+  else{
+    slideTo(currentImage + 1);
+  }   
+}
+
+window.onload = init;
