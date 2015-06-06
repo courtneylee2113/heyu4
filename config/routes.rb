7a68e340f5c5f5582b55fa1a1c40e5219d78a8d5
ikhakoo <ikhakoo@gmail.com>
2015-06-06 14:59:08 -0400

    removed devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { 
-  	registrations: "users/registrations",
-  	sessions: 'users/sessions' 
-  	}, :paths => 'users'
   root 'pages#home'
-
-  devise_scope :user do
-    get '/signout', to: 'devise/sessions#destroy', as: :signout
-  end
 
   resources :pins
   resources :pages
